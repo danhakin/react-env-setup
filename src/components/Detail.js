b39091f012ac4002dc439b046bd50ab337a3d7fa
Daniel Villalobos <mdvillalobos@gmail.com>
2016-05-07 16:49:42 +0200

    Multiple elements render using chance and props data

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -3,7 +3,13 @@ import Chance from 'chance';
 
 class Detail extends React.Component {
 	render() {
-		return <p>Hello, {chance.first()} <br /> {this.props.message}</p>
+		return ( 
+			<div>
+				<p>Hello, {chance.first()} </p> 
+				<p>You are from {chance.country({ full: true})}.</p> 
+				<p>{this.props.message}</p>
+			</div>
+		);
 	}
 }
 
