908a78aed96c1ba66bc637f5503f6717d31d2cca
Daniel Villalobos <mdvillalobos@gmail.com>
2016-05-07 17:05:12 +0200

    Added onclick event handling to Detail component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -2,12 +2,19 @@ import React from 'react';
 import Chance from 'chance';
 
 class Detail extends React.Component {
+
+	buttonClicked() {
+		console.log('Button was clicked');
+		this.forceUpdate();
+	}
+
 	render() {
 		return ( 
 			<div>
 				<p>Hello, {chance.first()} </p> 
 				<p>You are from {chance.country({ full: true})}.</p> 
 				<p>{this.props.message}</p>
+				<button onClick={this.buttonClicked.bind(this)}>Update Detail</button>
 			</div>
 		);
 	}
