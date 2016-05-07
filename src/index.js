467dd9af338283af6fa12f8c0eee5c78e4b3f72d
Daniel Villalobos <mdvillalobos@gmail.com>
2016-05-07 15:50:34 +0200

    Added a new React component - Detail

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Detail from "./components/Detail";
+
 console.log('React is up and running!');
+
+ReactDOM.render(<Detail />, document.getElementById('app'));
