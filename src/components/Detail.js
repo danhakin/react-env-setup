a44f920a0f202e9c4d4c1cec64c330d69f889496
Daniel Villalobos <mdvillalobos@gmail.com>
2016-05-10 22:34:26 +0200

    added superagent library and implemented basic ajax communication

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,45 +1,39 @@
 import React from 'react';
-import Chance from 'chance';
+import ajax from 'superagent';
 
 class Detail extends React.Component {
 
 	constructor(props){
 		super(props);
 
-		const people = [];
-
-		for (let i = 0; i < 10; i++) {
-			people.push({
-				name: chance.first(),
-				country: chance.country({full:true})
-			});
-		}
-
-		this.state = { people };
+		this.state = { commits: [] };
 	}
 
-	buttonClicked() {
-		console.log('Button was clicked');
-		const newState = {
-			name: chance.first()
-		};
-
-		// INFO: using setState merges the original state with the new one 
-		// and cause the component to re-render unless you name a function
-		// shouldUpdateComponent and returns false;
-		this.setState(newState);
+	componentWillMount() {
+		ajax.get('https://api.github.com/repos/facebook/react/commits')
+			.end((error, response) => {
+				if (!error && response) {
+					console.dir(response.body);
+					this.setState({ commits: response.body });
+				} else {
+					console.log('There was an error fetching from Github', error);
+				}
+			});
 	}
 
 	render() {
 		return ( 
 			<div>
-				<p><strong>{this.props.message}</strong></p>
-			{this.state.people.map((person,index) => (
-				<div key={index}>
-					<p>Hi {person.name} from {person.country} </p> 
-				</div> 
-			))}
-				<button onClick={this.buttonClicked.bind(this)}>Update Detail</button>
+				{this.state.commits.map((commit, index) => {
+					const author = commit.author ? commit.author.login : 'Anonymous';
+
+					return (
+					<p key={index}>
+						<strong>{author}</strong>
+						<a href="{commit.html_url}">{commit.commit.message}</a>
+					</p>
+					);
+				})}
 			</div>
 		);
 	}
