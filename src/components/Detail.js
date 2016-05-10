3363bb2ce52f152e3ab006e3ec1fb8a8cc5f2faf
Daniel Villalobos <mdvillalobos@gmail.com>
2016-05-10 21:21:39 +0200

    Added state to the component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -3,16 +3,32 @@ import Chance from 'chance';
 
 class Detail extends React.Component {
 
+	constructor(props){
+		super(props);
+
+		this.state = {
+			name: chance.first(),
+			country: chance.country({ full:true })
+		};
+	}
+
 	buttonClicked() {
 		console.log('Button was clicked');
-		this.forceUpdate();
+		const newState = {
+			name: chance.first()
+		};
+
+		// INFO: using setState merges the original state with the new one 
+		// and cause the component to re-render unless you name a function
+		// shouldUpdateComponent and returns false;
+		this.setState(newState);
 	}
 
 	render() {
 		return ( 
 			<div>
-				<p>Hello, {chance.first()} </p> 
-				<p>You are from {chance.country({ full: true})}.</p> 
+				<p>Hello, {this.state.name} </p> 
+				<p>You are from {this.state.country}.</p> 
 				<p>{this.props.message}</p>
 				<button onClick={this.buttonClicked.bind(this)}>Update Detail</button>
 			</div>
