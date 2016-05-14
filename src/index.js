05a756d5621cc08f043e0a2ae63c7d0566da82c2
Daniel Villalobos <mdvillalobos@gmail.com>
2016-05-14 16:17:54 +0200

    Created new List component and added a new route to index page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
 import { createHashHistory } from 'history';
 
+import List from "./components/List";
 import Detail from "./components/Detail";
 
 console.log('React is up and running!');
@@ -13,7 +14,8 @@ const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 ReactDOM.render(
 	<Router history={appHistory} onUpdate={ () => window.scrollTo(0,0) }>
-		<Route path="/" component={Detail} />
+		<Route path="/" component={ List } />
+		<Route path="/react" component={ Detail } />
 	</Router>,
 	document.getElementById('app')
 );
