8bd24c5954c0bef86e2d3d50a6a97cfbd8909fdd
Daniel Villalobos <mdvillalobos@gmail.com>
2016-05-14 16:08:43 +0200

    Integrated react-router and router histroy to the app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
+
 import Detail from "./components/Detail";
 
 console.log('React is up and running!');
 
+// Initialize an appHistory to handle hash histroy and deactivate query state.
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+
 ReactDOM.render(
-	<Detail message="This is coming from props!" />, 
+	<Router history={appHistory} onUpdate={ () => window.scrollTo(0,0) }>
+		<Route path="/" component={Detail} />
+	</Router>,
 	document.getElementById('app')
 );
