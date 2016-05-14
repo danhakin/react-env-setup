a868b091897dbec0cedda215a2fe6977e71c59a1
Daniel Villalobos <mdvillalobos@gmail.com>
2016-05-14 20:54:27 +0200

    Added App component and included a default IndexRoute in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { Router, Route, IndexRoute, useRouterHistory } from 'react-router';
 import { createHashHistory } from 'history';
 
+import App from "./components/App";
 import List from "./components/List";
 import Detail from "./components/Detail";
 
@@ -14,8 +14,10 @@ const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 ReactDOM.render(
 	<Router history={appHistory} onUpdate={ () => window.scrollTo(0,0) }>
-		<Route path="/" component={ List } />
-		<Route path="/detail/:repo" component={ Detail } />
+		<Route path="/" component={ App }>
+			<IndexRoute component={ List } />
+			<Route path="detail/:repo" component={ Detail } />
+		</Route>
 	</Router>,
 	document.getElementById('app')
 );
